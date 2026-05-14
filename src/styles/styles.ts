cf90d21dcf7fa452d1fcdda28d41594ef2326b5f
fabbbiodc <fabiodicecca@gmail.com>
2026-05-14 16:01:06 +0200

    Update styles.ts

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -4,4 +4,4 @@ export const homeSectionStyle =
   "h-screen w-full flex flex-col items-center justify-center p-4";
 
 export const containerStyle =
-  "relative w-full bg-bg-primary border-x-6 border-b-6 p-12 border-double";
+  "relative w-full h-full bg-bg-primary border-x-6 border-b-6 p-12 border-double";
