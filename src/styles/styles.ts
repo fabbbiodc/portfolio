1de61fde1e818a416787513d12d13316dc413d9a
fabbbiodc <fabiodicecca@gmail.com>
2026-05-14 15:59:40 +0200

    creating containers

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,2 +1,7 @@
+export const titlebarStyle = "px-4 py-1 w-full bg-bg-dark flex items-center text-text-bright"
+
+export const homeSectionStyle =
+  "h-screen w-full flex flex-col items-center justify-center p-4";
+
 export const containerStyle =
-  "bg-bg-primary ml-4 border-4 p-12 border-double max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full";
+  "relative w-full bg-bg-primary border-x-6 border-b-6 p-12 border-double";
