55f28bb5a7c9f07b89a2b2fe626cfabe6c3ac33c
fabbbiodc <fabiodicecca@gmail.com>
2026-05-25 12:06:16 +0200

    titlebar fix

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -9,9 +9,10 @@ export const containerContent =
 
 export const containerStyle = "w-1/2 h-full";
 
-export const draggableWindowStyle = "absolute top-0 left-0 w-[calc(100vw-2rem)] max-w-lg md:w-auto";
+export const draggableWindowStyle =
+  "absolute top-0 left-0 w-[calc(100vw-2rem)] max-w-lg md:w-auto";
 
 export const titlebarCloseButtonStyle =
-  "ml-auto p-3 min-w-[44px] min-h-[44px] flex items-center justify-center hover:bg-bg-light rounded transition-colors cursor-pointer";
+  "ml-auto min-w-[28px] min-h-[28px] flex items-center justify-center hover:bg-bg-light rounded transition-colors cursor-pointer";
 
 export const titlebarDragHandleStyle = "cursor-grab select-none touch-none";
