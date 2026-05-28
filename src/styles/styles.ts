d84d59ffeac0e06f44caa5ec3817ca2807739582
fabbbiodc <fabiodicecca@gmail.com>
2026-05-28 16:20:22 +0200

    shadows implemented

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -5,12 +5,12 @@ export const homeSectionStyle =
   "relative h-screen w-full flex flex-col items-center justify-center p-12";
 
 export const containerContent =
-  "w-auto h-auto bg-bg-primary border-x-6 border-b-6 border-double overflow-auto";
+  "w-auto h-auto bg-bg-primary border-x-6 border-b-6 border-double overflow-auto shadow-lg";
 
 export const containerStyle = "w-1/2 h-full";
 
 export const draggableWindowStyle =
-  "absolute top-0 left-0 w-[calc(100vw-2rem)] max-w-lg md:w-auto";
+  "absolute top-0 left-0 w-[calc(100vw-2rem)] max-w-lg md:w-auto shadow-window";
 
 export const titlebarCloseButtonStyle =
   "ml-auto min-w-[28px] min-h-[28px] flex items-center justify-center hover:bg-bg-light rounded transition-colors cursor-pointer";
