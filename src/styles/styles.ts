5af3685065323f3b31167703605175385a951795
fabbbiodc <fabiodicecca@gmail.com>
2026-06-05 14:48:33 +0200

    refactoring windows

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,16 +1,11 @@
 export const titlebarStyle =
   "px-4 py-1 w-full bg-bg-dark flex items-center text-text-bright";
 
-export const homeSectionStyle =
-  "relative h-screen w-full flex flex-col items-center justify-center p-12";
-
 export const containerContent =
-  "w-auto h-auto bg-bg-primary border-x-6 border-b-6 border-double shadow-lg";
+  "bg-bg-primary border-x-6 border-b-6 border-double shadow-lg";
 
-export const containerStyle = "w-1/2 h-full";
-
-export const draggableWindowStyle =
-  "absolute top-0 left-0 w-[calc(100vw-2rem)] max-w-lg shadow-window";
+export const windowStyle =
+  "absolute top-0 left-0 min-w-80 max-w-[calc(100vw-2rem)] max-h-[90vh] shadow-window flex flex-col";
 
 export const titlebarCloseButtonStyle =
   "ml-auto min-w-[28px] min-h-[28px] flex items-center justify-center hover:opacity-60";
@@ -36,7 +31,3 @@ export const markdownContentStyle =
 // Typography
 export const titlebarTextStyle = "text-sm md:text-base";
 export const captionTextStyle = "text-text-grey text-xs";
-export const h1TextStyle = "text-xl md:text-2xl";
-export const h2TextStyle = "text-lg md:text-xl";
-export const h3TextStyle = "text-base md:text-lg";
-export const bodyTextStyle = "text-sm md:text-base";
